d30e701740f09b4b7cc0e0bb9bc885d073f6ccc2
Tom Blaymire <tblaymire1994@gmail.com>
2017-12-11 21:15:10 +0000

    Implemented book detail action and reducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -4,13 +4,12 @@ import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
-
     renderList() {
         return this.props.books.map((book) =>{
             return (
                 <li
                     key={book.title}
-                    onClick={() => }
+                    onClick={() => this.props.selectBook(book)}
                     className="list-group-item">
                     {book.title}</li>
             );
